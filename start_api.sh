fa7f08bb5c0fdb14fb8ae4c342cc43bcb7a314af
AlexandreVig <alexandre.vigneau@epitech.eu>
2024-02-04 18:45:03 +0100

    [ADD] new implementation

--- start_api.sh
+++ start_api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SOURCE="/var/www/competition-analyzer.fr/public_html/api/"
+SOURCE="/var/www/competition-analyzer.fr/public_html/review-scraper/"
 echo "Start the websocket server"
 sudo python3 ${SOURCE}server.py

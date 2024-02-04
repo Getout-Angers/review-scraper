c8de3b187d77a34f42fa0497fa6c6d817dfa886d
AlexandreVig <alexandre.vigneau@epitech.eu>
2024-02-04 21:42:17 +0100

    update

--- src/scraper.py
+++ src/scraper.py
@@ -289,13 +289,11 @@ def scrape_places(driver: AntiDetectDriver, data, client=None, server=None):
                             sleep(sleep_time)
     
     search_link = create_search_link(data['query'], data['lang'], data['geo_coordinates'], data['zoom'])
-    if server is not None:
-        print("step_1")
-        server.send_message(client, "step_1")
+    print("step_1")
+    server.send_message(client, "step_1")
     perform_visit(driver, search_link)
-    if server is not None:
-        print("step_2")
-        server.send_message(client, "step_2")
+    print("step_2")
+    server.send_message(client, "step_2")
     
     set_cookies(driver.get_cookies_dict())
     
